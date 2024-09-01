d3027deeed3b73ff7b0b3c87995378b99776ba7a
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-09-01 15:55:32 +0300

    Hide title bar, but fix window in place.

--- simple_on_screen_counter/simple_counter.py
+++ simple_on_screen_counter/simple_counter.py
@@ -3,44 +3,40 @@ import keyboard
 import sys
 from tkinter import *
 from tkinter import messagebox
- 
+
  
 root = Tk()
 # Make background transparent.
 root.configure(bg="")
 
 # Places the window on the screen.
-root.geometry("+250+250")
+# Tinker with this until it's in the spot you want.
+root.geometry("+1650+830")
 
 # Brings window on top of all other windows.
 root.wm_attributes("-topmost", True)
-root.wm_attributes("-transparentcolor", "white")
+root.wm_attributes("-transparentcolor", "black")
 
+# Window size + title.
 root.geometry("300x250")
 root.title("Simple Counter")
 
-# Make window not resizable.
+# Make window not resizable + fixed in place.
 root.resizable(width=False, height=False)
+root.overrideredirect(True)
   
 # Declaration of variables
 counter_strvar = StringVar()
 counter_strvar.set("0")
 
-label = Label(root, textvariable=counter_strvar)
-label.pack(ipadx=10, ipady=10)
-
-
+label = Label(root, textvariable=counter_strvar, bg='black', fg='red', font=("Courier", 88))
+label.pack(ipadx=100, ipady=100)
 
 global counter
 global MAX_COUNTER_VAL
 
 counter = 0
 MAX_COUNTER_VAL = 99
-
-# User input - not needed for now.  
-# secondEntry= Entry(root, width=3, font=("Arial",18,""),
-#                    textvariable=second)
-# secondEntry.place(x=180,y=20)
   
 def increment_counter():
     global counter
