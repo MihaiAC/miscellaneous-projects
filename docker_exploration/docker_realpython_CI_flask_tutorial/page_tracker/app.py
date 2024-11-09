050a2afc98c561ee386a2182ce9a1cf94a8b46c6
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-09 18:38:18 +0200

    Add simple unit test. Add cache folders to .gitignore.

--- docker_exploration/docker_realpython_CI_flask_tutorial/page_tracker/app.py
+++ docker_exploration/docker_realpython_CI_flask_tutorial/page_tracker/app.py
@@ -1,10 +1,15 @@
+from functools import cache
+
 from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis()
 
 @app.get("/")
 def index():
-    page_view_count = redis.incr("page_view_count")
+    page_view_count = redis().incr("page_views")
     return f"This page has been seen {page_view_count} times."
+
+@cache
+def redis():
+    return Redis()
