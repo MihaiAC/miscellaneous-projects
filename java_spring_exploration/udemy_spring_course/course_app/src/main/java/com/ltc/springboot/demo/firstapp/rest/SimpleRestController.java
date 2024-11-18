36bf616c93f0152ae2710e4ae7246f3f64818557
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-18 19:37:59 +0200

    Add example of injecting properties.

--- java_spring_exploration/udemy_spring_course/course_app/src/main/java/com/ltc/springboot/demo/firstapp/rest/SimpleRestController.java
+++ java_spring_exploration/udemy_spring_course/course_app/src/main/java/com/ltc/springboot/demo/firstapp/rest/SimpleRestController.java
@@ -1,10 +1,23 @@
 package com.ltc.springboot.demo.firstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
+    // Example of injecting properties.
+    @Value("${some.property}")
+    private String someProperty;
+
+    @Value("${another.property}")
+    private String anotherProperty;
+
+    @GetMapping("/see_injected_properties")
+    public String seeProperties() {
+        return someProperty + " " + anotherProperty;
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello world!";
@@ -15,4 +28,6 @@ public class SimpleRestController {
     public String simpleDevtoolsTest() {
         return "It works!";
     }
+
 }
+
