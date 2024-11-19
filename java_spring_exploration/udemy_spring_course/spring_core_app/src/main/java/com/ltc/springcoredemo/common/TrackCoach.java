a8a3c1def556bb027d45842a1c4b341dcdca05a0
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-19 18:38:05 +0200

    Demonstrate lazy bean initialization.

--- java_spring_exploration/udemy_spring_course/spring_core_app/src/main/java/com/ltc/springcoredemo/common/TrackCoach.java
+++ java_spring_exploration/udemy_spring_course/spring_core_app/src/main/java/com/ltc/springcoredemo/common/TrackCoach.java
@@ -1,11 +1,15 @@
 package com.ltc.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@Lazy
 public class TrackCoach implements Coach {
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Do some track training.";
