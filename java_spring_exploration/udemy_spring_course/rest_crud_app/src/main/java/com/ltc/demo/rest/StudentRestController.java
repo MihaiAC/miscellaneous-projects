3c71931217b1a38c333f4e315512b0d0db41b04a
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-21 16:52:42 +0200

    Refactor code to load data after the endpoint is constructed.

--- java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/demo/rest/StudentRestController.java
+++ java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.ltc.demo.rest;
 
 import com.ltc.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+
+    private List<Student> students;
+
+    // Define @PostConstruct to load the student data.
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
 
         students.add(new Student("Firstname1", "Lastname1"));
         students.add(new Student("Firstname2", "Lastname2"));
         students.add(new Student("Firstname3", "Lastname3"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
 }
