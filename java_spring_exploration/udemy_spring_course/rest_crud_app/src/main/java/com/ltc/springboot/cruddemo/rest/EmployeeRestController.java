1ca4738d52050d54e30d14d5d30b919a89adf30f
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-21 20:32:34 +0200

    Add the REST endpoints supporting CRUD operations.

--- java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/springboot/cruddemo/rest/EmployeeRestController.java
+++ java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.ltc.springboot.cruddemo.dao.EmployeeDAO;
 import com.ltc.springboot.cruddemo.entity.Employee;
 import com.ltc.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +24,33 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable int employeeId) {
+        employeeService.deleteById(employeeId);
+    }
+
+
 }
