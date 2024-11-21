707ec0011e0a6a7af5555179b4af17ea9fbc97ab
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-21 20:09:26 +0200

    Add support for CRUD operations to EmployeeDAO.

--- java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -22,4 +22,22 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
         TypedQuery<Employee> query = entityManager.createQuery("FROM Employee", Employee.class);
         return query.getResultList();
     }
+
+    @Override
+    public Employee findById(int employeeId) {
+        Employee employee = entityManager.find(Employee.class, employeeId);
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        Employee dbEmployee = entityManager.merge(employee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int employeeId) {
+        Employee employee = entityManager.find(Employee.class, employeeId);
+        entityManager.remove(employee);
+    }
 }
