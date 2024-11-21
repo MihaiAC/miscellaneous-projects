73a35bd2a05d0669bbe82c854c21321385ce3aef
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-21 17:06:55 +0200

    Add endpoint for retrieving an object based on ID.

--- java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/demo/rest/StudentRestController.java
+++ java_spring_exploration/udemy_spring_course/rest_crud_app/src/main/java/com/ltc/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.ltc.demo.rest;
 import com.ltc.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,10 @@ public class StudentRestController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
