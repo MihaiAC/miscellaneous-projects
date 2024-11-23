a9226ce6c3c37b6b909dfb37405aa519af5d71eb
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-23 16:10:01 +0200

    Submit a java object using a form.

--- java_spring_exploration/udemy_spring_course/thymeleaf_demo/src/main/java/com/ltc/springboot/thymeleafdemo/controller/StudentController.java
+++ java_spring_exploration/udemy_spring_course/thymeleaf_demo/src/main/java/com/ltc/springboot/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.ltc.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,5 +18,11 @@ public class StudentController {
         model.addAttribute("student", student);
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student) {
+
+        return "student-confirmation";
     }
 }
