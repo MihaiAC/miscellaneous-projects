635de5a23790f651fb91c1779f9863443394df37
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-23 14:27:57 +0200

    Add data to the Model and display it.

--- java_spring_exploration/udemy_spring_course/thymeleaf_demo/src/main/java/com/ltc/springboot/thymeleafdemo/controller/BasicFormController.java
+++ java_spring_exploration/udemy_spring_course/thymeleaf_demo/src/main/java/com/ltc/springboot/thymeleafdemo/controller/BasicFormController.java
@@ -1,6 +1,8 @@
 package com.ltc.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,21 @@ public class BasicFormController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "basicformresponse";
+    }
+
+    // Controller method that reads form data and adds data to the model.
+    @RequestMapping("/processFormVersionTwo")
+    public String capitaliseFormInput(HttpServletRequest request, Model model) {
+        // Read user input.
+        String name = request.getParameter("studentName");
+
+        // Capitalise user input.
+        String capitalised_name = name.toUpperCase();
+
+        // Create a model variable containing the transformed input.
+        model.addAttribute("message", capitalised_name);
+
         return "basicformresponse";
     }
 }
