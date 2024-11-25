43cf19b6437b017f37d6a30d5d7e96926491189b
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-25 21:07:30 +0200

    Add form to add an employee to the db.

--- java_spring_exploration/udemy_spring_course/mvc_rest_crud_demo/src/main/java/com/ltc/springboot/thymeleafdemo/controller/EmployeeController.java
+++ java_spring_exploration/udemy_spring_course/mvc_rest_crud_demo/src/main/java/com/ltc/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,4 +29,16 @@ public class EmployeeController {
         return "list-employees";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/list";
+    }
 }
