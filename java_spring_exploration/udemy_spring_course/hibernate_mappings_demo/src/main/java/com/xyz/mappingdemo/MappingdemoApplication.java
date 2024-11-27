7c36e1e3a2c9822d34208ef336b192985339d0eb
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-27 17:59:57 +0200

    Create InstructorDetail class and map it to the appropriate SQL table.

--- java_spring_exploration/udemy_spring_course/hibernate_mappings_demo/src/main/java/com/xyz/mappingdemo/MappingdemoApplication.java
+++ java_spring_exploration/udemy_spring_course/hibernate_mappings_demo/src/main/java/com/xyz/mappingdemo/MappingdemoApplication.java
@@ -1,13 +1,22 @@
 package com.xyz.mappingdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MappingdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MappingdemoApplication.class, args);
+	}
+
+    @Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("placeholder");
+		};
 	}
 
 }
