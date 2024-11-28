ad3ac32437fb2aa34429bb099a55d38f33e2f68b
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-28 16:16:20 +0200

    Create a Course class and establish the one-to-many relation between the Instructor and Course objects.

--- java_spring_exploration/udemy_spring_course/hibernate_mappings_demo/src/main/java/com/xyz/mappingdemo/entity/Instructor.java
+++ java_spring_exploration/udemy_spring_course/hibernate_mappings_demo/src/main/java/com/xyz/mappingdemo/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.xyz.mappingdemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="instructor")
 public class Instructor {
@@ -23,6 +26,10 @@ public class Instructor {
     @JoinColumn(name="instructor_detail_id")
     private InstructorDetail instructorDetail;
 
+    @OneToMany(mappedBy="instructor", cascade={CascadeType.DETACH, CascadeType.MERGE,
+                                                CascadeType.MERGE, CascadeType.PERSIST})
+    private List<Course> courses;
+
     public Instructor() {
 
     }
@@ -33,8 +40,25 @@ public class Instructor {
         this.email = email;
     }
 
+    public void add(Course course) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(course);
+        course.setInstructor(this);
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     public void setEmail(String email) {
