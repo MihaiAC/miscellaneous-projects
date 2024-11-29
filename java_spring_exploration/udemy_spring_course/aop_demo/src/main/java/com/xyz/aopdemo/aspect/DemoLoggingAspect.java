c81db48daeded03dc149693db737e0d7529377e7
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-29 16:57:16 +0200

    Add one more matching example.

--- java_spring_exploration/udemy_spring_course/aop_demo/src/main/java/com/xyz/aopdemo/aspect/DemoLoggingAspect.java
+++ java_spring_exploration/udemy_spring_course/aop_demo/src/main/java/com/xyz/aopdemo/aspect/DemoLoggingAspect.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class DemoLoggingAspect {
+    // Match any method in a package.
+    // @Before("execution(* com.xyz.aopdemo.dao.*.*(..))")
+
+    // Match any function starting with "add" that takes an Account object as param.
     @Before("execution(* add*(com.xyz.aopdemo.Account))")
     public void beforeAddAccountAdvice() {
         System.out.println("Executing @Before advice on addAccount");
