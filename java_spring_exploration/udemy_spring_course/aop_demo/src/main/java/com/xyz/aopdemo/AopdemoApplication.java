32d2dbe67318d9e1f5fef0be7ff9c406195aa597
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-29 15:20:51 +0200

    Complete aop_demo initialisation.

--- java_spring_exploration/udemy_spring_course/aop_demo/src/main/java/com/xyz/aopdemo/AopdemoApplication.java
+++ java_spring_exploration/udemy_spring_course/aop_demo/src/main/java/com/xyz/aopdemo/AopdemoApplication.java
@@ -1,10 +1,19 @@
 package com.xyz.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Placeholder.");
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AopdemoApplication.class, args);
