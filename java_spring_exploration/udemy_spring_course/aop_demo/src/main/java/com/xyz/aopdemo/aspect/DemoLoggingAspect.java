07f0b6406586b08f9447eedab27307dda4e7249f
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-11-29 17:00:37 +0200

    Add a pointcut declaration.

--- java_spring_exploration/udemy_spring_course/aop_demo/src/main/java/com/xyz/aopdemo/aspect/DemoLoggingAspect.java
+++ java_spring_exploration/udemy_spring_course/aop_demo/src/main/java/com/xyz/aopdemo/aspect/DemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.xyz.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -11,7 +12,12 @@ public class DemoLoggingAspect {
     // @Before("execution(* com.xyz.aopdemo.dao.*.*(..))")
 
     // Match any function starting with "add" that takes an Account object as param.
-    @Before("execution(* add*(com.xyz.aopdemo.Account))")
+    // @Before("execution(* add*(com.xyz.aopdemo.Account))")
+
+    @Pointcut("execution(* com.xyz.aopdemo.dao.*.*(..))")
+    private void pointcutDaoPackage() { }
+
+    @Before("pointcutDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("Executing @Before advice on addAccount");
     }
