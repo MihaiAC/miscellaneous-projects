8bdb78141e65811f38a159b040311ce8969d61f7
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-02 20:40:00 +0200

    Use redirect.

--- django_exploration/django_practical_guide/monthly_challenges/challenges/views.py
+++ django_exploration/django_practical_guide/monthly_challenges/challenges/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpRequest
+from django.http import HttpResponse, HttpResponseNotFound, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.utils.dates import MONTHS
 
 def monthly_challenge_by_number(request: HttpRequest, month: int):
     if 1 <= month <= 12:
-        return HttpResponse(f"Placeholder challenge for {MONTHS[month]}")
+        return HttpResponseRedirect(f"/challenges/{MONTHS[month]}")
     return HttpResponseNotFound("Invalid month.")
 
 def monthly_challenge(request: HttpRequest, month: str):
