a453624a144532feda2f58e01c119c772c85130f
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-05 20:19:08 +0200

    Add aggregated stats to the template.

--- django_exploration/django_practical_guide/django_models/book_outlet/views.py
+++ django_exploration/django_practical_guide/django_models/book_outlet/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
+from django.db.models import Avg
 from .models import Book
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
     books = Book.objects.all()
+
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": books.count(),
+        "average_rating": books.aggregate(Avg("rating"))
     })
 
 def book_detail(request: HttpRequest, id: int) -> HttpResponse:
