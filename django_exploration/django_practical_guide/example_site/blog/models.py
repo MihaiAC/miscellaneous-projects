2ef6c3f681973bf786e07749c06bc8b5de701ecb
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-06 20:25:17 +0200

    Make Author's email_address EmailField().

--- django_exploration/django_practical_guide/example_site/blog/models.py
+++ django_exploration/django_practical_guide/example_site/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email_address = models.CharField(max_length=255)
+    email_address = models.EmailField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
