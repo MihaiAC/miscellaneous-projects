7813552770864da2bfc4c45fc47588b31e5706fc
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-06 18:54:24 +0200

    Add models to the blog example.

--- django_exploration/django_practical_guide/example_site/blog/models.py
+++ django_exploration/django_practical_guide/example_site/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=255)
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=255)
+    image_name = models.CharField(max_length=255)
+    date = models.DateField()
+    excerpt = models.TextField(default="", null=False, blank=True)
+    slug = models.SlugField(default="", null=False, blank=True)
+    content = models.TextField(default="", null=False, blank=True)
+    
+    # 1-to-many
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts", null=False)
+
+    # many-to-many
+    tag = models.ManyToManyField(Tag)
