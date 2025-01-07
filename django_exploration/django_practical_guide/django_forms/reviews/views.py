b81cc3ce03d914cfc5db63ba0d9fb3a968df6c85
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-07 23:42:14 +0200

    Add a class-based view.

--- django_exploration/django_practical_guide/django_forms/reviews/views.py
+++ django_exploration/django_practical_guide/django_forms/reviews/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.views import View
 
 from .forms import ReviewForm
 # Create your views here.
 
-# request can be POST or GET;
-def review(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
+# Class-based view.
+class ReviewView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        form = ReviewForm()
+    
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+    
+    def post(self, request: HttpRequest) -> HttpResponse:
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewForm()
-    
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
 def thank_you(request: HttpRequest) -> HttpResponse:
     return render(request, "reviews/thank_you.html")
