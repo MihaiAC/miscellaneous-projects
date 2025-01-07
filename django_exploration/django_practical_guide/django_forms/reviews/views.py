2d842ea22127ea08b9b490ef7b0d43857487aac0
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-07 23:21:36 +0200

    Save the form data in a model.

--- django_exploration/django_practical_guide/django_forms/reviews/views.py
+++ django_exploration/django_practical_guide/django_forms/reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 
 from .forms import ReviewForm
+from .models import Review
 # Create your views here.
 
 # request can be POST or GET;
@@ -10,7 +11,10 @@ def review(request: HttpRequest) -> HttpResponse:
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            review = Review(user_name=form.cleaned_data['user_name'],
+                            review_text=form.cleaned_data['review_text'],
+                            rating = form.cleaned_data['rating'])
+            review.save()
             return HttpResponseRedirect("/thank-you")
 
         return render(request, "reviews/review.html", {
