b4a807e1f24e2f9dc0026f2d4929d3333d81da71
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-07 19:45:42 +0200

    Add manual form validation.

--- django_exploration/django_practical_guide/django_forms/reviews/views.py
+++ django_exploration/django_practical_guide/django_forms/reviews/views.py
@@ -7,10 +7,18 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 def review(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         entered_username = request.POST['username']
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })
+
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
         
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
 
 def thank_you(request: HttpRequest) -> HttpResponse:
     return render(request, "reviews/thank_you.html")
