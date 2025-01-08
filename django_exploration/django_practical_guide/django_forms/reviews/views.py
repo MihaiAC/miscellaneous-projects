1ae4bbdd4710a945092ea876c5a5371d4b4e1094
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-08 18:15:45 +0200

    Use a TemplateView with a basic html template.

--- django_exploration/django_practical_guide/django_forms/reviews/views.py
+++ django_exploration/django_practical_guide/django_forms/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 # Create your views here.
@@ -25,5 +26,11 @@ class ReviewView(View):
             "form": form
         })
 
-def thank_you(request: HttpRequest) -> HttpResponse:
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'This works!'
+        return context
+    
