209e8c79ddd2da5d549cb9a4f433c4a5e2e22170
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-08 19:35:27 +0200

    Save simple image upload.

--- django_exploration/django_practical_guide/django_forms/profiles/views.py
+++ django_exploration/django_practical_guide/django_forms/profiles/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.core.files.uploadedfile import UploadedFile
+
+def store_file(file: UploadedFile):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 # Create your views here.
 class CreateProfileView(View):
@@ -8,5 +14,5 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        print(request.FILES["image"])
+        store_file(request.FILES["image"])
         return HttpResponseRedirect("/profiles")
