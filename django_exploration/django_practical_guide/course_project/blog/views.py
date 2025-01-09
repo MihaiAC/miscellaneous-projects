a1eb21119a097b21ed2e95a7f41aac86b82abf29
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-09 23:28:29 +0200

    Add stored posts feature.

--- django_exploration/django_practical_guide/course_project/blog/views.py
+++ django_exploration/django_practical_guide/course_project/blog/views.py
@@ -8,6 +8,7 @@ from django.views import View
 from django.urls import reverse
 
 class StartingPageView(ListView):
+    print("ASDOJAPSDKOASD")
     template_name = "blog/index.html"
     model = Post
     ordering = ["-date"]
@@ -26,14 +27,28 @@ class AllPostsView(ListView):
 
 # Automatically searches by slug and raises 404 if not found.
 class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = str(post_id) in stored_posts
+        else:
+            is_saved_for_later = False
+        
+        return is_saved_for_later
+
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
         context = {
             "post": post,
             "tags": post.tag.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
+
+
         return render(request, "blog/post-detail.html", context)
     
     def post(self, request, slug):
@@ -51,7 +66,40 @@ class SinglePostView(View):
             "post": post,
             "tags": post.tag.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
 
         return render(request, "blog/post-detail.html", context)
+
+class ReadLaterView(View):
+    # Retrieve the stored posts.
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        
+        return render(request, "blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+        
+        post_id = request.POST["post_id"]
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+        request.session["stored_posts"] = stored_posts
+        
+        return HttpResponseRedirect("/")
