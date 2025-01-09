5812bb9a3f2aae9784f02202230b40749222d07d
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-09 20:35:56 +0200

    Add the Comment model and form.
    Allow submitting comments for each post and display them.

--- django_exploration/django_practical_guide/course_project/blog/admin.py
+++ django_exploration/django_practical_guide/course_project/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Tag, Author
+from .models import Post, Tag, Author, Comment
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
@@ -16,5 +16,4 @@ class AuthorAdmin(admin.ModelAdmin):
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Author, AuthorAdmin)
-
-
+admin.site.register(Comment)
--- django_exploration/django_practical_guide/course_project/blog/views.py
+++ django_exploration/django_practical_guide/course_project/blog/views.py
@@ -1,8 +1,11 @@
 from datetime import date
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from .models import Post, Tag, Author
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from .forms import CommentForm
+from django.views import View
+from django.urls import reverse
 
 class StartingPageView(ListView):
     template_name = "blog/index.html"
@@ -22,12 +25,33 @@ class AllPostsView(ListView):
     context_object_name = "all_posts"
 
 # Automatically searches by slug and raises 404 if not found.
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "tags": post.tag.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tags"] = self.get_object().tag.all()
-        return context
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return HttpResponseRedirect(reverse("single-post-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "tags": post.tag.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
 
+        return render(request, "blog/post-detail.html", context)
