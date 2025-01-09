6b3688171fd2ba4139b2ca1fa117b08ee0f019ed
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-09 18:59:52 +0200

    Migrate function views to class-based views.

--- django_exploration/django_practical_guide/course_project/blog/views.py
+++ django_exploration/django_practical_guide/course_project/blog/views.py
@@ -2,23 +2,32 @@ from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from .models import Post, Tag, Author
+from django.views.generic import ListView, DetailView
 
-def starting_page(request: HttpRequest) -> HttpResponse:
-    posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-        "posts": posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-def posts(request: HttpRequest) -> HttpResponse:
-    posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-def single_post(request: HttpRequest, slug: str) -> HttpResponse:
-    identified_post = Post.objects.get(slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "tags": identified_post.tag.all()
-    })
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# Automatically searches by slug and raises 404 if not found.
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = self.get_object().tag.all()
+        return context
 
