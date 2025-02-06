793aeb5158632c830ffad571e39329c4b83f0e1e
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-06 21:46:16 +0200

    Add password and confirm password validation.

--- frontend_exploration/odinproject/JS/form_validation/src/index.js
+++ frontend_exploration/odinproject/JS/form_validation/src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 import { EmailValidator } from "./email_validation";
 import { PostcodeValidator } from "./postcode_validation";
+import { PasswordValidator } from "./password_validation";
 
 document.addEventListener("DOMContentLoaded", () => {
   window.emailValidator = new EmailValidator();
   window.postcodeValidator = new PostcodeValidator();
+  window.passwordValidator = new PasswordValidator();
 });
---
+++ frontend_exploration/odinproject/JS/form_validation/src/password_validation.js
@@ -0,0 +1,111 @@
+export class PasswordValidator {
+  static #regexSpecialChar = new RegExp('[!@#$%^&*(),.?":{}|<>]');
+  static #regexNumber = new RegExp("\\d");
+  static #regexUppercase = new RegExp("[A-Z]");
+  static #regexLowercase = new RegExp("[a-z]");
+
+  constructor() {
+    this.form = document.querySelector("form");
+    this.password = document.getElementById("password");
+    this.confirmPassword = document.getElementById("confirm-password");
+    this.passwordError = document.querySelector("#password + span.error");
+    this.confirmPasswordError = document.querySelector(
+      "#confirm-password + span.error"
+    );
+
+    // Password validity.
+    this.password.addEventListener("input", (event) => {
+      this.validatePassword();
+      this.validateConfirmPassword();
+    });
+
+    // Confirm password validity.
+    this.confirmPassword.addEventListener("input", (event) => {
+      this.validateConfirmPassword();
+    });
+
+    // Prevent form submission if password fields are not valid.
+    this.form.addEventListener("submit", (event) => {
+      if (!this.passwordIsValid()) {
+        this.showPasswordError();
+        event.preventDefault();
+      } else if (!this.confirmPasswordIsValid()) {
+        this.showConfirmPasswordError();
+        event.preventDefault();
+      }
+    });
+  }
+
+  validatePassword() {
+    if (this.passwordIsValid()) {
+      this.passwordError.textContent = "";
+      this.passwordError.className = "error";
+    } else {
+      this.showPasswordError();
+    }
+  }
+
+  validateConfirmPassword() {
+    if (this.confirmPasswordIsValid()) {
+      this.confirmPasswordError.textContent = "";
+      this.confirmPasswordError.className = "error";
+    } else {
+      this.showConfirmPasswordError();
+    }
+  }
+
+  validate() {
+    if (this.isValid()) {
+      this.postcodeError.textContent = "";
+      this.postcodeError.className = "error";
+    } else {
+      this.showError();
+    }
+  }
+
+  passwordIsValid() {
+    return (
+      PasswordValidator.#regexLowercase.test(this.password.value) &&
+      PasswordValidator.#regexNumber.test(this.password.value) &&
+      PasswordValidator.#regexSpecialChar.test(this.password.value) &&
+      PasswordValidator.#regexUppercase.test(this.password.value) &&
+      this.password.validity.valid
+    );
+  }
+
+  confirmPasswordIsValid() {
+    return this.password.value == this.confirmPassword.value;
+  }
+
+  showPasswordError() {
+    // Handle password errors.
+    if (this.password.validity.valueMissing) {
+      this.passwordError.textContent = "You need to enter a password.";
+    } else if (this.password.validity.tooShort) {
+      this.passwordError.textContent =
+        "Password needs to have minimum 10 characters.";
+    } else if (this.password.validity.tooLong) {
+      this.passwordError.textContent =
+        "Password needs to have maximum 30 characters.";
+    } else if (!PasswordValidator.#regexLowercase.test(this.password.value)) {
+      this.passwordError.textContent =
+        "Password needs to have at least one lowercase character.";
+    } else if (!PasswordValidator.#regexUppercase.test(this.password.value)) {
+      this.passwordError.textContent =
+        "Password needs to have at least one uppercase character.";
+    } else if (!PasswordValidator.#regexSpecialChar.test(this.password.value)) {
+      this.passwordError.textContent =
+        "Password needs to have at least one special character.";
+    } else if (!PasswordValidator.#regexNumber.test(this.password.value)) {
+      this.passwordError.textContent =
+        "Password needs to have at least one number.";
+    }
+    this.passwordError.className = "error active";
+  }
+
+  showConfirmPasswordError() {
+    // Handle confirm password error.
+    this.confirmPasswordError.textContent = "Must match the password field.";
+    this.confirmPasswordError.className = "error active";
+  }
+}
