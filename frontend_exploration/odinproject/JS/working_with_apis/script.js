676783d77c3fe57f19aad94b268491f0f0699880
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-07 20:51:41 +0200

    Add Weather class for displaying the response.

--- frontend_exploration/odinproject/JS/working_with_apis/script.js
+++ frontend_exploration/odinproject/JS/working_with_apis/script.js
@@ -23,7 +23,28 @@ class WeatherGetter {
   async getWeatherData(location) {
     const url = this.buildUrl(location);
     let response = await fetch(url);
-    let json = await response.json();
-    console.log(json);
+    let jsonResponse = await response.json();
+    return jsonResponse;
+  }
+}
+
+class Weather {
+  static #address = document.getElementById("address");
+  static #conditions = document.getElementById("conditions");
+  static #cloudcover = document.getElementById("cloudcover");
+  static #temp = document.getElementById("tempC");
+
+  constructor(resolvedAddress, conditions, cloudcover, temp) {
+    this.address = resolvedAddress;
+    this.conditions = conditions;
+    this.cloudcover = cloudcover;
+    this.tempC = (((temp - 32) * 5) / 9).toFixed(2);
+  }
+
+  displayWeather() {
+    Weather.#address.textContent = this.address;
+    Weather.#conditions.textContent = this.conditions;
+    Weather.#cloudcover.textContent = this.cloudcover + "%";
+    Weather.#temp.textContent = this.tempC + "°C";
   }
 }
