2e30d25c58adcac44c3469b136deff72d0ae93de
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-07 20:20:28 +0200

    Add code for querying the weather API.

--- frontend_exploration/odinproject/JS/working_with_apis/script.js
+++ frontend_exploration/odinproject/JS/working_with_apis/script.js
@@ -1,1 +1,29 @@
-console.log("Linked");
+class WeatherGetter {
+  static #urlPrefix =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+  #apiKey;
+
+  constructor(apiKey) {
+    this.#apiKey = apiKey;
+  }
+
+  buildUrl(location) {
+    const today = new Date().toISOString().split("T")[0];
+    const url =
+      WeatherGetter.#urlPrefix +
+      "/" +
+      location +
+      "/" +
+      today.toString() +
+      "?key=" +
+      this.#apiKey;
+    return url;
+  }
+
+  async getWeatherData(location) {
+    const url = this.buildUrl(location);
+    let response = await fetch(url);
+    let json = await response.json();
+    console.log(json);
+  }
+}
