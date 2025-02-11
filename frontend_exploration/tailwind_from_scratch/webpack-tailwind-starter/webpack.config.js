ddf3b99d4898018d23e009b41c10bd0c0ff3e363
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-11 02:05:31 +0200

    Switch from tailwindcss@4 to tailwindcss@3.

--- frontend_exploration/tailwind_from_scratch/webpack-tailwind-starter/webpack.config.js
+++ frontend_exploration/tailwind_from_scratch/webpack-tailwind-starter/webpack.config.js
@@ -1,29 +1,55 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, "src"),
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
+module.exports = (env) => {
+  return {
+    entry: "./src/index.js",
+    mode: env.prod ? "production" : "development",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
+    devServer: env.prod
+      ? {}
+      : {
+          static: {
+            directory: path.join(__dirname, "dist"),
+          },
+          hot: true,
+          port: 8080,
+          open: true,
+        },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      clean: true,
+      filename: "[name].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env"]],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+            { loader: "postcss-loader" },
+          ],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+          type: "asset",
+        },
+      ],
+    },
+  };
 };
